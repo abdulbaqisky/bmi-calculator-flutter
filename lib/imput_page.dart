d4a492be4336f1d538af3514caf3d7e1210b1b47
“[Adekomi <adekomiabdulbaqi3@gmail.com>
2020-10-11 19:24:51 +0100

    encapsulation] added fontAwesoneIcon updated container 1 UI

--- lib/imput_page.dart
+++ lib/imput_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class InputPage extends StatefulWidget {
   @override
@@ -18,7 +19,25 @@ class _InputPageState extends State<InputPage> {
             child: Row(
               children: [
                 Expanded(
-                  child: NewContainer(colour: Color(0xFF1D1E33)),
+                  child: NewContainer(
+                    colour: Color(0xFF1D1E33),
+                    cardChild: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(FontAwesomeIcons.mars, size: 80.0),
+                        SizedBox(
+                          height: 15.0,
+                        ),
+                        Text(
+                          'MALE',
+                          style: TextStyle(
+                            fontSize: 18.0,
+                            color: Color(0XFF8D8E98),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
                 ),
                 Expanded(
                   child: NewContainer(colour: Color(0xFF1D1E33)),
@@ -48,13 +67,15 @@ class _InputPageState extends State<InputPage> {
 }
 
 class NewContainer extends StatelessWidget {
-  NewContainer({@required this.colour});
+  NewContainer({@required this.colour, this.cardChild});
 
   final Color colour;
+  final Widget cardChild;
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      child: cardChild,
       margin: EdgeInsets.all(15.0),
       decoration: BoxDecoration(
         color: colour,
@@ -63,3 +84,14 @@ class NewContainer extends StatelessWidget {
     );
   }
 }
+
+/*
+ReUse(
+iconMe: Icon(FontAwesomeIcons.mars),
+what: Text('MALE')),
+class ReUse extends StatelessWidget {
+  ReUse({this.what, this.iconMe});
+
+  final Text what;
+  final Icon iconMe;
+*/
