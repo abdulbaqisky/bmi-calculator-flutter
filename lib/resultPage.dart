5ff8c649803c1da5f9779582ba04dfe8a174b55d
“[Adekomi <adekomiabdulbaqi3@gmail.com>
2020-10-16 00:14:21 +0100

    ResultPage UI complete

--- lib/resultPage.dart
+++ lib/resultPage.dart
@@ -39,12 +39,33 @@ class ResultPage extends StatelessWidget {
                   ),
                   Text(
                     'Your BMI is quite Low you should eat more',
+                    textAlign: TextAlign.center,
                     style: kBodyTextStyle,
                   ),
                 ],
               ),
             ),
-          )
+          ),
+          Expanded(
+            child: GestureDetector(
+              onTap: () {
+                Navigator.pop(context);
+              },
+              child: Container(
+                child: Center(
+                  child: Text(
+                    'RE-CALCULATE',
+                    style: kLargeButtonTextStyle,
+                  ),
+                ),
+                padding: EdgeInsets.only(bottom: 20.0),
+                margin: EdgeInsets.only(top: 10.0),
+                height: kBottomContainerHeight,
+                width: double.infinity,
+                color: kBottomContainerColor,
+              ),
+            ),
+          ),
         ],
       ),
     );
